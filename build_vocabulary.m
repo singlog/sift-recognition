ea730a6820f48ac5a41084f59ca87b9073e5baf0
singlog <zyrgzfxk@hotmail.com>
2018-10-08 18:56:47 -0700

    Update build_vocabulary.m

--- build_vocabulary.m
+++ build_vocabulary.m
@@ -1,3 +1,11 @@
+%{
+Created on 05/20/2018
+
+Run this script to build a bag-of-words description for images with
+pre-computed sift features for each frame.
+The script first build a vocabulary of descriptors,
+i.e. classify the descriptors into clusters.
+%}
 
 siftdir = './sift/';
 framesdir = './frames/';
@@ -5,6 +13,9 @@ framesdir = './frames/';
 
 fnames = dir([siftdir '/*.mat']);
 numframe = length(fnames);
+
+%build the feature vocabulary from a random sample of size 200 (frames).
+%In this case, the number 200 produces a desriable result.
 
 rand = randperm(numframe);
 rand = rand(1:200);
@@ -17,6 +28,9 @@ for i=rand
     feat = [feat; descriptors];
 end
 
+%classify the features into 1500 clusters
+%save the mean parameters for later sift descriptors classification.
+
 [member,means,~] = kmeansML(1500,feat');
 means = means';
 member = member';
@@ -26,6 +40,10 @@ imwords = zeros(numframe,1500);
 names = zeros(numframe,23);
 
 bin = 1:1500;
+
+%for each frame, classify the descriptors into 1500 bins.
+%save the frame as a histogram of the bins.
+%save all the frame data into one .mat file.
 
 for i=1:numframe
    
