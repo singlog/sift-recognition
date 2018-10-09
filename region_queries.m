23724bdd7c20f1dc8e92cbb4bf4aefdf51b1dd6e
singlog <zyrgzfxk@hotmail.com>
2018-10-08 19:06:17 -0700

    Update region_queries.m

--- region_queries.m
+++ region_queries.m
@@ -1,3 +1,18 @@
+%{
+Created on 05/23/2018
+
+This script illustrates using sift features and bag-of-words 
+representationfor instance recognition.
+
+User can select a polygon region on a popped up image and 
+the script findsthe other images with the similar region.
+The sift feature description is shown to be translation 
+invariant and scaleinvariant.
+
+The bag-of-words representation and the visual vocabulary 
+is pre-computed with build_vocalubary.m
+%}
+
 clear;
 load words.mat;
 load kmeans.mat;
@@ -6,6 +21,7 @@ framedir = './frames/';
 siftdir = './sift/';
 bin = 1:1500;
 
+%the frames used for illustrating
 frame_names = ["0000003652.jpeg","0000000401.jpeg","0000005806.jpeg","0000003553.jpeg"];
 
 for i = 1:4
@@ -20,6 +36,8 @@ for i = 1:4
     ptsin = inpolygon(positions(:,1),positions(:,2),polygon(:,1),polygon(:,2));
     ind = find(ptsin == 1);
     feats = descriptors(ind,:);
+    
+    %find the frame with the most similar visual word histogram
     
     dist = dist2(feats,means);
     [~,minind] = min(dist,[],2);
